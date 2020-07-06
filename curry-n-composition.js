b46ad261f30d3f14acc15736817a69fda4b14b09
abdelp <juniorperezpy@gmail.com>
2020-07-06 11:22:55 -0400

    Add curry and composition example

---
+++ curry-n-composition.js
@@ -0,0 +1,46 @@
+const map = fn => mappable => mappable.map(fn);
+
+const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
+const log = (...args) => console.log(...args);
+
+const arr = [1, 2, 3, 4];
+const isEven = n => n % 2 === 0;
+
+const stripe = n => isEven(n) ? 'dark' : 'light';
+const stripeAll = map(stripe);
+const striped = stripeAll(arr);
+
+log(striped);
+
+const double = n => n * 2;
+const doubleAll = map(double);
+const doubled = doubleAll(arr);
+log(doubled);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- reducer.js
+++ reducer.js
@@ -1,6 +1,18 @@
+const { filter } = require("lodash");
+
 const reduced = [1, 2, 3, 4, 5, 6, 7, 8].reduce((acc, curr) => {
   curr % 2 == 0 ? acc.push(curr * 2) : 0;
   return acc;
 }, []);
 
 console.log(reduced);
+
+const censor = words => filter(
+  word => word.length !== 4,
+  words
+);
+
+const startsWithS = words => filter(
+  word => word.startsWithS('s'),
+  words
+);
