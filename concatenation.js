dd6fb6b9f62ab63a39243b21d788778bfaa0570a
abdelp <juniorperezpy@gmail.com>
2020-07-07 09:52:14 -0400

    Add concatenation example

---
+++ concatenation.js
@@ -0,0 +1,15 @@
+const objs = [
+  {a: 'a', b: 'ab'},
+  {b: 'b'},
+  {c: 'c', b: 'cb'}
+];
+
+const concatenate = (a, o) => ({...a,...o});
+
+const c = objs.reduce(concatenate, {});
+
+console.log(
+  'concatenation',
+  c,
+  `enumerable keys: ${Object.keys(c)}`
+);
