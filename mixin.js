34de36e1d884a7620e8780a21840242d9c81be12
abdelp <juniorperezpy@gmail.com>
2020-07-07 15:29:03 -0400

    Add mixin example

---
+++ mixin.js
@@ -0,0 +1,19 @@
+const chocolate = {
+  hasChocolate: () => true
+};
+
+const caramelSwirl = {
+  hasCaramelSwirl: () => true
+};
+
+const pecans = {
+  hasPecans: () => true
+};
+
+const iceCream = { ...chocolate, ...caramelSwirl, ...pecans };
+
+console.log(`
+  hasChocolate: ${iceCream.hasChocolate()}
+  hasCaramelSwirl: ${iceCream.hasCaramelSwirl()}
+  hasPecans: ${iceCream.hasPecans()}
+`);
